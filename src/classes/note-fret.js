679e1b358fc55517f01e4862cb160807a57a764b
thomaspantioras <t.pantioras@gmail.com>
2021-04-14 16:17:58 +0100

    displayed frets

--- src/classes/note-fret.js
+++ src/classes/note-fret.js
@@ -1,25 +1,32 @@
 export default class NoteFret {
-  // constructor(className) {
-  //   this.noteFretClassName = className;
-  //   this._noteName = "";
-  // }
-  // get noteName() {
-  //   if (this._noteName) return this._noteName;
-  //   return;
-  // }
-  // set noteName(value) {
-  //   this._noteName = value;
-  // }
-  // createFretTemplate() {
-  //   const noteFret = document.createElement("div");
-  //   noteFret.classList.add(this.noteFretClassName);
-  //   return noteFret;
-  // }
-  // addSingleMarkOnFret = (noteFretEl, singleMarkClassName) =>
-  //   noteFretEl.classList.add(singleMarkClassName);
-  // addDoubleMarkOnFret = (noteFretEl, doubleMarkClassName) => {
-  //   const doubleFretMark = document.createElement("div");
-  //   doubleFretMark.classList.add(doubleMarkClassName);
-  //   noteFretEl.appendChild(doubleFretMark);
-  // };
+  constructor() {
+    this._className = '';
+    this._noteName = '';
+  }
+  get className() {
+    if (this._className) return this._className;
+    return;
+  }
+  set className(value) {
+    this._className = value;
+  }
+  get noteName() {
+    if (this._noteName) return this._noteName;
+    return;
+  }
+  set noteName(value) {
+    this._noteName = value;
+  }
+  createFretTemplate() {
+    const noteFret = document.createElement('div');
+    noteFret.classList.add(this._className);
+    return noteFret;
+  }
+  addSingleMarkOnFret = (noteFretEl, singleMarkClassName) =>
+    noteFretEl.classList.add(singleMarkClassName);
+  addDoubleMarkOnFret = (noteFretEl, doubleMarkClassName) => {
+    const doubleFretMark = document.createElement('div');
+    doubleFretMark.classList.add(doubleMarkClassName);
+    noteFretEl.appendChild(doubleFretMark);
+  };
 }
--- src/controllers/FretboardCtrl.js
+++ src/controllers/FretboardCtrl.js
@@ -1,9 +1,15 @@
 import Fretboard from '../classes/fretboard';
 import InstrumentString from '../classes/instrument-string';
-import { NoteFretCtrl } from './NoteFretCtrl';
+import NoteFret from '../classes/note-fret';
+// import { NoteFretCtrl } from './NoteFretCtrl';
 // import { StringCtrl } from "../controllers/StringCtrl.js";
 
 const FretboardCtrl = (function () {
+  // const noteFretClassName = 'note-fret'; // does this member belong in NoteFret class?
+  // const singleMarkClassName = 'single-fretmark'; // does this member belong in NoteFret class?
+  // const singleMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
+  // const doubleMarkClassName = 'double-fretmark'; // does this member belong in NoteFret class?
+  // const doubleMarkPositions = [12, 24];
   const noteFlat = [
     'C',
     'Db',
@@ -70,23 +76,38 @@ const FretboardCtrl = (function () {
     return stringInstance.createStringElement();
   };
 
+  const createFretElement = (className) => {
+    const noteFretInstance = new NoteFret();
+    noteFretInstance.className = className;
+    return noteFretInstance.createFretTemplate();
+  };
+
+  // const addFretMark = function (fretIndex, noteFret) {
+  //   if (doubleMarkPositions.indexOf(fretIndex) !== -1)
+  //     noteFret.addDoubleMarkOnFret(noteFret, 'double-fretmark');
+  //   if (singleMarkPositions.indexOf(fretIndex) !== -1)
+  //     noteFret.addSingleMarkOnFret(noteFret, 'single-fretmark');
+  // };
+
   const buildFretboard = function (
     fretboardUiSelector,
     numberOfStrings,
     numberOfFrets
   ) {
-    // const fretboardInstance = createFretboardObject(fretboardUiSelector);
     setCssRootVariable('--number-of-strings', numberOfStrings);
     const fretboardHtmlElement = getEmptyFretboardHtmlElement(
       fretboardUiSelector
     );
     for (let i = 0; i < numberOfStrings; i++) {
-      // setCssRootVariable('--string-height', 10 - i);
       let string = createStringElement(`string${i}`);
       fretboardHtmlElement.appendChild(string);
       for (let fret = 0; fret <= numberOfFrets; fret++) {
-        console.log('noteFret');
-        //       let noteFret = NoteFretCtrl.createFretElement();
+        let noteFret = createFretElement('note-fret');
+        noteFret.style.setProperty('--note-fret-width', `${70 - fret * 2.5}px`);
+        string.appendChild(noteFret);
+        // if (i === 0) addFretMark(fret, noteFret);
+
+        // let noteFret = NoteFretCtrl.createFretElement();
         //       string.appendChild(noteFret);
         //       if (i === 0) NoteFretCtrl.addFretMark(fret, noteFret);
         //       let noteName = NoteFretCtrl.getFretNoteName(
